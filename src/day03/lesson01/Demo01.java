b580739ca58cecc8d849217b6e1b9dc7e5e1bcd5
tangsong41 <369223985@qq.com>
2019-08-14 23:28:50 +0800

    array-opp01

---
+++ src/day03/lesson01/Demo01.java
@@ -0,0 +1,33 @@
+package day03.lesson01;
+
+public class Demo01 {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/day03/lesson03for/Demo01.java
@@ -0,0 +1,39 @@
+package day03.lesson03for;
+
+public class Demo01 {
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for(int i=0;i<100;i++) {
+			 sum += i;
+			 
+			
+		}
+		System.out.println(sum);
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
