d5babefd159ba927644a5f07effa9abaa75c7950
unknown <369223985@qq.com>
2021-02-20 17:19:03 +0800

    string

--- src/day12common/stu_String/TestStringDemo01.java
+++ src/day12common/stu_String/TestStringDemo01.java
@@ -4,7 +4,11 @@ public class TestStringDemo01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//3.1 获取指定位置字符
+		String str = "I love Java";
+		System.out.println("长度是："+str.length());
+		char c = str.charAt(7);
+		System.out.println("索引位置为7的字符为：" + c);
 		
 		
 		
--- src/day12common/stu_StringBuilder/StringBuilderDemo01.java
+++ src/day12common/stu_StringBuilder/StringBuilderDemo01.java
@@ -4,11 +4,7 @@ public class StringBuilderDemo01 {
 	
 	public static void main(String[] args) {
 		
-		//3.1 获取指定位置字符
-		String str = "I love Java";
-		System.out.println("长度是："+str.length());
-        char c = str.charAt(7);
-        System.out.println("索引位置为7的字符为：" + c);
+
         
         
         
