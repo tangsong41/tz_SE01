cde633a9065a8d559c18cc872580aa13f17b8103
unknown <369223985@qq.com>
2021-02-23 09:40:53 +0800

    StringBuilder

--- src/day12common/stu_StringBuilder/StringBuilderDemo03.java
+++ src/day12common/stu_StringBuilder/StringBuilderDemo03.java
@@ -14,12 +14,24 @@ public class StringBuilderDemo03 {
          */
 
 
+
         System.out.println("---------------------");
         /*
             String  转化为  Stringbuilder
             构造方法
          */
+        String strs[] = {"Arshad", "Althamas", "Johar", "Javed", "Raju", "Krishna" };
+        StringBuilder sb = new StringBuilder();
+        sb.append(strs[0]);
+        sb.append(" "+strs[1]);
+        sb.append(" "+strs[2]);
+        sb.append(" "+strs[3]);
+        sb.append(" "+strs[4]);
+        sb.append(" "+strs[5]);
 
+        System.out.println("sb = " + sb);
+        System.out.println("---");
+        System.out.println(sb.toString());
 
 
 
