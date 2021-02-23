6667530c39ff5fad60b65846ece2365de170341d
unknown <369223985@qq.com>
2021-02-23 09:27:41 +0800

    StringBuilder反转

--- src/day12common/stu_StringBuilder/StringBuilderTest02.java
+++ src/day12common/stu_StringBuilder/StringBuilderTest02.java
@@ -1,5 +1,7 @@
 package day12common.stu_StringBuilder;
 
+import java.util.Scanner;
+
 /**
  * @Author: tangsong
  * @Date: 2021-02-22 23:35
@@ -10,12 +12,23 @@ public class StringBuilderTest02 {
         /*
             字符串 反转 升级
          */
-
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("qingshuru: ");
+        String dd = sc.nextLine();
+        String ddf = resverse(dd);
+        System.out.println(ddf);
 
     }
 
+    public static String resverse(String arr){
+        StringBuilder sb = new StringBuilder();
 
+//        sb += "["
+//
+//        sb += "]"
+        String ss = sb.append(arr).reverse().toString();
+        return ss ;
+    }
 
 
 
